a417655093db5f0254dfbd88e68f67a4f08affcf
João Gabriel <joao.conceicao@monitoratec.com.br>
2018-08-29 17:00:49 -0300

    My First Commit

---
+++ Games/Games.swift
@@ -0,0 +1,9 @@
+//
+//  Games.swift
+//  Games
+//
+//  Created by João Gabriel  on 29/08/2018.
+//  Copyright © 2018 fontana. All rights reserved.
+//
+
+import Foundation
---
+++ Games/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Games
+//
+//  Created by João Gabriel  on 29/08/2018.
+//  Copyright © 2018 fontana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
